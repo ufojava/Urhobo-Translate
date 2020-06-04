557364e71303d4ceb136603f0407714c0cafd3c4
Ufuoma Okoro <ufookoro@gmail.com>
2020-06-04 08:41:51 +0100

    Initial Commit

---
+++ Urhobo Translate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Urhobo Translate
+//
+//  Created by Ufuoma Okoro on 04/06/2020.
+//  Copyright © 2020 Ufuoma Okoro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
